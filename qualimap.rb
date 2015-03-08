1585fe5094f928c4004a03a4ab7664deea280cbc
Lorena <lorena.pantano@gmail.com>
2015-03-08 13:21:20 -0400

    add version

--- qualimap.rb
+++ qualimap.rb
@@ -4,7 +4,8 @@ class Qualimap < Formula
   homepage 'http://qualimap.bioinfo.cipf.es/'
   url 'https://bitbucket.org/kokonech/qualimap/downloads/qualimap-build-06-03-15.tar.gz'
   sha1 'fc0a227d2739a1e92b95cfabee074dc54d4c7ce6'
-
+  version '06-03-15'
+  
   depends_on 'r' => :optional
 
   def install
